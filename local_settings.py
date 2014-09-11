d104f0d10066756fbea18c18644b8752a1ff1721
Master <master@Pikachu.(none)>
2014-09-11 10:42:50 +0800

    local settings

--- local_settings.py
+++ local_settings.py
@@ -1,2 +1,2 @@
-BROKER_URL = 'mongodb://localhost:27017/helloCeleryBroker'
-BACKEND_URL = 'mongodb://localhost:27017/helloCeleryBackend'
+BROKER_URL = 'mongodb://140.116.21.177:27017/helloCeleryBroker'
+BACKEND_URL = 'mongodb://140.116.21.177:27017/helloCeleryBackend'
